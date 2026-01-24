b9afa9661c6194ff0b12fcf6fd105583e330f4f3
Aigerim <aigerim@MacBook-Air-Aigerim.local>
2026-01-24 11:28:25 +0500

    practice1

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,56 @@
+x = 1j
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+
+
+
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x(list):
+print(type(x)) 
+
+
+
+
+
+x = ("apple", "banana", "cherry")
+
+#display x:
+print(x)
+
+#display the data type of x(tuple):
+print(type(x)) 
+
+
+
+
+
+x = {"name" : "John", "age" : 36}
+
+#display x:
+print(x)
+
+#display the data type of x(dict):
+print(type(x)) 
+
+
+
+
+
+
+x = {"apple", "banana", "cherry"}
+
+#display x:
+print(x)
+
+#display the data type of x(set):
+print(type(x)) 
---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,31 @@
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+
+
+
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+
+
+
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+
+
+
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+
+
+
+txt = "We are the so-called \"Vikings\" from the north."
