f91f04a4880db73ec6c9c6b3f9a69a304f231129
Aigerim <aigerim@MacBook-Air-Aigerim.local>
2026-01-31 11:23:38 +0500

    commit

--- Practice1/python-basics/strings.py
+++ Practice1/python-basics/strings.py
@@ -28,4 +28,97 @@ print(b[-5:-2])
 
 
 
-txt = "We are the so-called \"Vikings\" from the north."
+
+
+s = 'hell45 world6'
+a = " "
+for i in s:
+  if i.isdigit():
+    a+=i
+print(a)
+    
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+
+
+
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+
+
+
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+
+
+
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+
+
+
+
+
+s = 'hell45 world6'
+a = " "
+for i in s:
+  if i.isdigit():
+    a+=i
+print(a)
+    
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+
+
+
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+
+
+
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+
+
+
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+
+
+
+
+
+s = 'hell45 world6'
+a = " "
+for i in s:
+  if i.isdigit():
+    a+=i
+print(a)
+    
+s = 'hell45 world6'
+a = " "
+for i in s:
+  if i.isdigit():
+    a+=i
+print(a)
+    
